7e57a9bfe8e409993703d8f3ec6313318b472a7f
Federico Gonzalia <federico.gonzalia@gmail.com>
2022-05-04 22:57:29 -0300

    adding letter_comparison.js

---
+++ letter_comparison.js
@@ -0,0 +1,21 @@
+// Return true if "word" contains all of the "letters"
+const mutation = arr => {
+    let word = arr[0].toLowerCase();
+    let letters = arr[1].toLowerCase();
+
+    for (const staticIdx in letters) {
+        for (let dinamicIdx in word) {
+            if (word[dinamicIdx] === letters[staticIdx] && letters.length - 1 != staticIdx) {
+                break;
+
+            } else if (word[dinamicIdx] === letters[staticIdx]) {
+                return true;
+
+            } else if (word[dinamicIdx] !== letters[staticIdx] && word.length - 1 == dinamicIdx) {
+                return false;
+            }
+        }
+    }
+}
+
+console.log(mutation(["hello", "Hello"]));
